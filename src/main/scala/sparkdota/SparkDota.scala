afd5977cb50c7fe536427067ff4d3fea9be133b6
hoangpham95 <phhoang@ccs.neu.edu>
2018-04-06 01:48:03 -0400

    Second try on parsing data.

--- src/main/scala/sparkdota/SparkDota.scala
+++ src/main/scala/sparkdota/SparkDota.scala
@@ -7,12 +7,36 @@ import org.apache.spark.sql.types._
 
 import scala.collection.mutable.ListBuffer
 import org.apache.spark.SparkContext
+import org.apache.spark.sql.SaveMode
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 
+case class Player(
+    account_id: Long,
+    player_slot: Long,
+    hero_id: Long,
+    kills: Long,
+    deaths: Long,
+    assists: Long,
+    leaver_status: Boolean,
+    last_hits: Long,
+    gold_per_min: Long,
+    xp_per_min: Long,
+    hero_damage: Long,
+    tower_damange: Long
+)
+
+case class Match(
+    game_mode: Long,
+    match_id: Long,
+    match_seq_num: Long,
+    duration: Long,
+    players: Seq[Player],
+    radiant_win: Boolean
+)
+
 object SparkDota {
-  val dataPath =
-    "/home/hpham/Desktop/dota.json" //"s3a://emrfs-dota-data/yasp-dump.json"
+  val dataPath = "s3a://emrfs-dota-data/yasp-dump.json"
 
   val spark: SparkSession =
     SparkSession
@@ -20,17 +44,16 @@ object SparkDota {
       .appName("Spark Dota")
       .config("spark.master", "local")
       .getOrCreate()
+
 // implicit conversions
   import spark.implicits._
   val sc: SparkContext = spark.sparkContext
   val hc = sc.hadoopConfiguration
   val awsCred = getAWSCred();
 
-  // println(awsCred)
-
   hc.set("fs.s3a.impl", "org.apache.hadoop.fs.s3native.NativeS3FileSystem")
-  hc.set("fs.s3a.awsAccessKeyId", awsCred(1))
-  hc.set("fs.s3a.awsSecretAccessKey", awsCred(2))
+  hc.set("fs.s3a.awsAccessKeyId", awsCred(0))
+  hc.set("fs.s3a.awsSecretAccessKey", awsCred(1))
 
   def main(args: Array[String]): Unit = {
     processData();
@@ -39,17 +62,70 @@ object SparkDota {
 
   def processData() {
     val rawDataRDD = sc
-      .wholeTextFiles(dataPath)
-      .map(_._2.replace("\n", "").trim)
+      .textFile(dataPath)
+      .take(5)
 
-    val df = spark.sqlContext.read.json(rawDataRDD)
-    val filtered = df
-      .select($"game_mode", $"players", $"radiant_win")
+    val data = sc.parallelize(rawDataRDD)
+
+    val df = spark.sqlContext.read.json(data)
+
+    val firstRound = df
+      .select(
+        $"game_mode",
+        $"match_id",
+        $"match_seq_num",
+        $"duration",
+        $"radiant_win",
+        // parsing players data
+        $"players.account_id".as("account_id"),
+        $"players.player_slot".as("player_slot"),
+        $"players.kills".as("kills"),
+        $"players.deaths".as("deaths"),
+        $"players.assists".as("assists"),
+        $"players.leaver_status".as("leaver_status"),
+        $"players.last_hits".as("last_hits"),
+        $"players.gold_per_min".as("gold_per_min"),
+        $"players.xp_per_min".as("xp_per_min"),
+        $"players.hero_damage".as("hero_damage"),
+        $"players.tower_damage".as("tower_damage")
+      )
       .where($"human_players" === 10)
       .where($"game_mode".isin("1", "2", "22"))
-      .where(!array_contains($"players.leaver_status", 1))
+      .where(!array_contains($"leaver_status", 1))
 
-    filtered.show()
+    firstRound.printSchema()
+    firstRound.show();
+
+    // val secondRound = firstRound
+    //   .map(
+    //     mat =>
+    //       Match(
+    //         mat.getLong(0), // game_mode
+    //         mat.getLong(1), // match_id
+    //         mat.getLong(2), // match_seq_num
+    //         mat.getLong(3), // duration
+    //         mat
+    //           .getAs[Seq[Row]](5)
+    //           .map(p =>
+    //             Player(
+    //               p.getLong(2), // account_id
+    //               p.getLong(41), // player_slot
+    //               p.getLong(19), // hero_id,
+    //               p.getLong(29), // kills
+    //               p.getLong(9), // deaths
+    //               p.getLong(4), // assists,
+    //               p.getLong(33) == 1, // leaver_status
+    //               p.getLong(32), // last_hits
+    //               p.getLong(12), // gold_per_min
+    //               p.getLong(49), // xp_per_min
+    //               p.getLong(16), // hero_damage
+    //               p.getLong(48) // tower_damange
+    //           )),
+    //         mat.getBoolean(4) // radiant_win
+    //     ))
+
+    // secondRound.printSchema();
+    // secondRound.show();
   }
 
   def getAWSCred(): List[String] = {
@@ -57,7 +133,7 @@ object SparkDota {
       val filename = "src/main/resources/sparkdota/rootkey.txt"
       Source.fromFile(filename).getLines.toList
     } catch {
-      case e => println("Path to the rootkey is not correct")
+      case _: Exception => List()
     }
   }
 }
